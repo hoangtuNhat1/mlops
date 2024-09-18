41273254ba4af2ea75e04e9f4c12c32ccbb774bf
hoangtuNhat1 <kimchanbn123@gmail.com>
2024-09-18 23:29:19 +0700

    3.2

--- 06-best-practices/code/lambda_function.py
+++ 06-best-practices/code/lambda_function.py
@@ -4,7 +4,7 @@ import os
 PREDICTIONS_STREAM_NAME = os.getenv('PREDICTIONS_STREAM_NAME')
 TEST_RUN = os.getenv('TEST_RUN')
 RUN_ID = os.getenv('RUN_ID')
-model_services = model.init(PREDICTIONS_STREAM_NAME, RUN_ID, "123",TEST_RUN)
+model_services = model.init(PREDICTIONS_STREAM_NAME, RUN_ID, RUN_ID,TEST_RUN)
 
 def lambda_handler(event, context):
     return model_services.lambda_handler(event)
